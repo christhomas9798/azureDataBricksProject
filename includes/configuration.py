3b78578dc4e7a851f6366b8c71d3f272a57076d9
christhomas9798 <101183889+christhomas9798@users.noreply.github.com>
2024-07-05 16:59:25 +0530

    Commit before pushing to remote repository

---
+++ includes/configuration.py
@@ -0,0 +1,7 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/f1dl9798/raw"
+processed_folder_path = "/mnt/f1dl9798/processed"
+presentation_folder_path = "/mnt/f1dl9798/presentation"
+
+# COMMAND ----------
+
